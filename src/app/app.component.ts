c51afaf80be05392c2963f6228194fc3716931f1
oumaimamajdoub <oumaima.majdoub@esprit.tn>
2023-02-16 18:16:27 +0100

    workshop1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testangular';
+  /*person: string ='Oumaima';
+  age:number=25;
+  address:any ={street:'rue du ...', city:'Ariana'};
+  hide:boolean=false;
+  activated: boolean=false;
+  hideInter: boolean=false;
+
+  getEmail() {
+    return this.person +'@gmail.com';
+    
+  }
+  hideInterpolation(){
+    this.hideInter=true;
+  }*/
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
